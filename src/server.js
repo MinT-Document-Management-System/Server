6287973b53be2e2760d5ad9a1ba0fb4639be2c4b
Ermias Muluken <ermiasmuluken321@gmail.com>
2024-12-23 13:39:11 +0300

    cors added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const Routes  = require('./routes/mainRoutes');
+const cors = require("cors")
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 require('dotenv').config();
 
@@ -17,4 +23,3 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -54,7 +54,10 @@ class UserService {
 
 
     async reset_password(userdata){
-        const {email, old_password, new_password} = userdata
+        const {email, old_password, new_password,confirm_password} = userdata
+        if (new_password !== confirm_password){
+            return {"success":false, "error":"Password doesn't match"}
+        }
         const user = await User.findOne({
             where: {
                 email: email,
