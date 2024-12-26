128f0b00028bd850176547b4ec66aa4e9880026c
Yonas W <yonaseth12@gmail.com>
2024-12-26 12:19:30 +0300

    Error handling for user services

--- src/controllers/userController/userControllers.js
+++ src/controllers/userController/userControllers.js
@@ -1,34 +1,44 @@
 const Userservice = require('../../services/userServices')
 
 const login = async function (req, res) {
-    const userdata = req.body
-    const email = userdata.email
-    const password = userdata.password
+    try{
+        const userdata = req.body
+        const email = userdata.email
+        const password = userdata.password
+    
+        const result = await Userservice.login_with_email(email, password)
+    
+        res.status(200).json(result)
 
-    const result = await Userservice.login_with_email(email, password)
-    if(!(result.success)){
-        if(result.error === "Temporary password is not reset."){
-            res.redirect("http://frontend_web:3000/reset_password_page")
-        }
+    } catch(error) {
+        res.status(error.status || 500).json({error: error.message})
     }
-
-    res.send(result)
 }
 
 const signup = async function (req, res) {
-    const userdata = req.body
+    try {
+        const userdata = req.body
+    
+        const newUser = await Userservice.createUser(userdata)
+    
+        res.status(201).json(newUser)
 
-    const newUser = await Userservice.createUser(userdata)
-
-    res.send(newUser)
+    } catch (error) {
+        res.status(error.status || 500).json({error: error.message})
+    }
 }
 
 const reset_password = async function (req, res) {
-    const userdata = req.body
+    try {
+        const userdata = req.body
+    
+        const token = await Userservice.reset_password(userdata)
+    
+        res.status(200).json(token)
 
-    const token = await Userservice.reset_password(userdata)
-
-    res.send(token)
+    } catch (error) {
+        res.status(error.status || 500).json({error: error.message})
+    }
 }
 
 const username_checker = async function (req, res) {
