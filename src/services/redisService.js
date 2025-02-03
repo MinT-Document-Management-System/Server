46066d28661ec96a2d92e7b9b0a45681e90678a0
Yonas W <yonaseth12@gmail.com>
2025-02-03 13:28:36 +0300

    fixed upstash configuration issue

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
-const { createClient } = require('ioredis');
+const Redis = require('ioredis');
 
-const redis_client = createClient({
-    url: `rediss://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-    tls: {
-        rejectUnauthorized: false // Required for Upstash
-    },
-    commandTimeout: 10000 // 10 seconds
-});
+const redis_client = new Redis(
+    `rediss://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+);
 
 redis_client.on('error', (err) => {
     console.error('Redis connection error:', err);
