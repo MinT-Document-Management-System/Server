b0a9477db19a2ce76d71d6a7562f8f0a1ea1aa86
Yonas W <yonaseth12@gmail.com>
2025-02-08 17:18:14 +0300

    add authentication middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+const jwt = require("jsonwebtoken");
+
+const authenticateJWT = (req, res, next) => {
+    // const token = req.header("Authorization")?.split(" ")[1]; // Extract token from Authorization header
+    const token = res.cookies.jwt_token
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized. You must login first." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // Attach user data to request object
+        next();
+    } catch (err) {
+        return res.status(403).json({ message: "Forbidden. Your token is invalid." });
+    }
+};
+
+module.exports = authenticateJWT;
