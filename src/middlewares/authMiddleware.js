1d74e8f594c43c326f1effe85cf7e8f4e2cdd27e
Yonas W <yonaseth12@gmail.com>
2025-02-09 11:43:08 +0300

    fix bugs

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ const authenticateJWT = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.user = decoded; // Attach user data to request object
         next();
     } catch (err) {
