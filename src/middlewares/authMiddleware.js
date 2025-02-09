d7632d5aae3b6766099058c5727cd372b384e128
Yonas W <yonaseth12@gmail.com>
2025-02-09 17:15:34 +0300

    attach useful data on request object

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,7 +1,9 @@
 require("dotenv").config()
 const jwt = require("jsonwebtoken");
+const { User, Department } = require('../models/many_to_many_models/user_department_relation')
+const Role = require('../models/roleModel')
 
-const authenticateJWT = (req, res, next) => {
+const authenticateJWT = async (req, res, next) => {
     // const token = req.header("Authorization")?.split(" ")[1]; // Extract token from Authorization header
     const token = req.cookies.jwt_token
 
@@ -11,10 +13,28 @@ const authenticateJWT = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        req.user = decoded; // Attach user data to request object
+
+
+        // Checking if user exists
+        const user = await User.findOne({
+            where: { user_id: decoded.user_id },
+            include: [
+                { model: Role, attributes: ['role_name'] },
+                { model: Department, as: 'Departments', attributes: ['department_name'] }
+             ]
+          });
+          
+        if (!user) {
+            return res.status(401).json({ message: "User not found." });
+        }
+
+        user.departments =  user.Departments.map(dept => dept.dataValues.department_name);
+        user.role_name   =  user.Role.dataValues.role_name;
+      
+        req.user = user; // Attach user data to request object
         next();
     } catch (err) {
-        return res.status(403).json({ message: "Forbidden. Your token is invalid." });
+        return res.status(403).json({ message: err.message });
     }
 };
 
