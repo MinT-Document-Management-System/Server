64849a84545a5e8efe29c723378bf9b669395f47
Yonas W <yonaseth12@gmail.com>
2025-02-10 23:20:56 +0300

    modify ingoing letter model and creation

--- src/models/ingoingModel.js
+++ src/models/ingoingModel.js
@@ -27,23 +27,18 @@ const Ingoing = sequelize.define("Ingoing",
         },
         date_written: {
             type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
             allowNull: true
         },
         date_forwarded: {
             type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
             allowNull: true
         },
         date_received: {
             type: DataTypes.DATE,
             allowNull: true
 
-        },
-        comment_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: Comment,
-                key: "comment_id"
-            }
         },
     },
     {
@@ -55,9 +50,6 @@ const Ingoing = sequelize.define("Ingoing",
 // Defining Relationship
 Ingoing.belongsTo(Letter_Document, { foreignKey: 'document_id', onDelete: 'CASCADE' });
 Letter_Document.hasOne(Ingoing, { foreignKey: 'document_id' });
-
-Ingoing.hasMany(Comment, { foreignKey: 'comment_id' });
-Comment.belongsTo(Ingoing, { foreignKey: 'comment_id',  onDelete: 'CASCADE' });
 
 // Ingoing.sync({alter: false})
 
--- src/services/ingoingServices.js
+++ src/services/ingoingServices.js
@@ -4,14 +4,17 @@ class IngoingServices {
 
     async create_ingoing( metadata, document_id){
         try {
-            const date_written = metadata.date_written || new Date().toLocaleString();
-            const date_forwarded = new Date().toLocaleString()
+            const letter_id = metadata.letter_id || "TBC by Foreign Sender"
             const sender_body = metadata.sender_body || metadata.title;
-            if(!date_forwarded||!date_written||!document_id||!sender_body){
+
+            const date_written = metadata.date_written || NULL
+            const date_forwarded = metadata.date_forwarded || NULL
+            
+            if( !document_id || !sender_body ) {
                 const error = new Error("Required fields are missing");
                 error.status = 400; throw error;
             }
-            const created_letter = await Ingoing.create({document_id,sender_body,date_forwarded,date_written})
+            const created_letter = await Ingoing.create({document_id, letter_id, sender_body, date_forwarded, date_written})
             return created_letter
 
         } catch (err) {
