2ae3b26ecbc13161b3e5b9312844fd672e3947e0
Yonas W <yonaseth12@gmail.com>
2025-02-11 02:48:49 +0300

    add authentication on all endpoints that require it

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const authenticateJWT = require('../middlewares/authMiddleware')
+const authenticateJWT = require('../middlewares/authN_Middleware')
 const role_routes = require("./roleRoutes")
 const department_routes = require("./departmentRoutes")
 const user_routes = require("./userRoutes")
@@ -8,12 +8,12 @@ const letter_routes = require("./letterRoutes")
 const main_router = express.Router()
 
 // Main Router
-main_router.use('/role', role_routes)
+main_router.use('/role', authenticateJWT, role_routes)
 // User Router
 main_router.use('/user', user_routes)
 // Letter Router
 main_router.use('/letter', authenticateJWT, letter_routes)
 // Department Router
-main_router.use('/department', department_routes)
+main_router.use('/department', authenticateJWT, department_routes)
 
 module.exports = main_router
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,30 +1,40 @@
 const express = require('express');
+const authenticateJWT = require('../middlewares/authN_Middleware')
 const { login, signup, reset_password, check_user_email, forget_password_send_otp, forget_password, username_checker, get_user_data, get_all_users, delete_user, get_recently_created_users } = require('../controllers/userController/userControllers');
 
 
 const router = express.Router();
 
+const router_public = express.Router();
+const router_restricted = express.Router();
+router_restricted.use(authenticateJWT)
+
 // Login route
-router.post('/login', login);
-// Signup route
-router.post('/signup', signup);
+router_public.post('/login', login);
 // Reset password route
-router.post('/reset_password', reset_password)
+router_public.post('/reset_password', reset_password)
 // Check if user exists route
-router.post('/check_user_email/:user_email', check_user_email)
+router_public.post('/check_user_email/:user_email', check_user_email)
 // Forget Password Send OTP
-router.post('/forget_password_send_otp', forget_password_send_otp)
+router_public.post('/forget_password_send_otp', forget_password_send_otp)
 // Forget password route
-router.post('/forget_password', forget_password)
+router_public.post('/forget_password', forget_password)
+
+// Signup route
+router_restricted.post('/signup', signup);
 // Unused username checker Route
-router.get('/username_check/:username', username_checker)
+router_restricted.get('/username_check/:username', username_checker)
 // Fetch a user
-router.get('/get_user_data/:user_id', get_user_data)
+router_restricted.get('/get_user_data/:user_id', get_user_data)
 // Fetch five recently created users
-router.get('/get_recently_created_users', get_recently_created_users)
+router_restricted.get('/get_recently_created_users', get_recently_created_users)
 // Fetch all users
-router.get('/get_all_users', get_all_users)
+router_restricted.get('/get_all_users', get_all_users)
 // Delete a user
-router.get('/delete_user/:user_id', delete_user)
+router_restricted.get('/delete_user/:user_id', delete_user)
+
+
+router.use(router_public)
+router.use(router_restricted)
 
 module.exports = router;
