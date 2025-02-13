bab7597d2c77ee339ca6746569d521fe8b842f7d
Yonas W <yonaseth12@gmail.com>
2025-02-13 23:44:38 +0300

    add endpoints to RBAC permission list

--- src/middlewares/role_based_access_permission.js
+++ src/middlewares/role_based_access_permission.js
@@ -1,21 +1,18 @@
 const RBAC = {
     user: {
-        general: [],
-        staff: [],
-        department_head: [],
-        record_official: []
+        // anyone: ['login', 'reset_password', 'check_user_email', 'forget_password_send_otp', 'forget_password'],   // Already hadndled by exclusion of any middleware for this paths
+        general: ['get_user_data']  // All except the it & admin.
     },
     letter: {
-        general: [],
-        staff: [],
-        department_head: [],
-        it: [],
+        general: ['upload_letter', 'get_letter', 'get_all_letters', 'upload_letter_version', 'request_letter_deletion'], // it has no access
+        department_head: ['grant_access', 'revoke_access'],
     },
     role: {
-        general: [], // staff, department_head, record_official
+        general: ['get_role_by_id', 'get_all_roles'], // staff, department_head, record_official
     },
     department: {
-        general: [],
+        general: ['get_all_departments', 'get_department_details'],
+        it: ['add_department', 'delete_department', 'get_user_count_by_department']
     }
 }
 
